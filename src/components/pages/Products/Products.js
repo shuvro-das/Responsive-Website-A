9306207c734cf258d16f808fadee16fd7e359bed
shuvro-das <shuvrodas1999@gmail.com>
2020-12-21 01:02:29 +0530

    Responsive Website

---
+++ src/components/pages/Products/Products.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeroSection from '../../HeroSection';
+
+import { homeObjTwo } from '../HomePage/Data';
+
+
+function Products() {
+    return (
+        <>
+                  
+                  
+                   
+                   <HeroSection {...homeObjTwo} />
+                  
+        </>
+    )
+}
+
+export default Products
---
+++ src/components/pages/SignUp/SignUp.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeroSection from '../../HeroSection';
+
+import { homeObjFour} from '../HomePage/Data';
+
+
+function SignUp() {
+    return (
+        <>
+                  
+                  
+                   
+                   <HeroSection {...homeObjFour} />
+                  
+        </>
+    )
+}
+
+export default SignUp
